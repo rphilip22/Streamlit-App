11ddde5d756a7d3bf1f63f12ca7146f6a8904d65
rphilip22 <reuben.philip@student.fairfield.edu>
2026-04-12 19:08:42 -0400

    updates

--- data_utils.py
+++ data_utils.py
@@ -7,7 +7,7 @@ file_name = "data.csv"
 def load_data():
     # Create file if it doesn't exist
     if not os.path.exists(file_name):
-        pd.DataFrame(columns=["Type", "Amount", "Category"]).to_csv(file_name, index=False)
+        pd.DataFrame(columns=["Type", "Amount", "Category", "Description"]).to_csv(file_name, index=False)
 
     return pd.read_csv(file_name)
 
